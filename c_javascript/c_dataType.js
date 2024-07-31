316f20f36a2759ecfa116dc0dc4f787c4ca2b882
shifttag <ehdgus0837@naver.com>
2024-07-31 09:28:38 +0900

    20240731

--- c_javascript/c_dataType.js
+++ c_javascript/c_dataType.js
@@ -57,3 +57,56 @@ console.log(typeof bool1);  // boolean
 
 console.log(bool2); // false
 console.log(typeof bool2);  // boolean
+
+//# 4. null & undefined 자료형
+
+// 4-1 null : 아무것도 없다.
+// >> 변수의 데이터가 유효하지 않은 경우
+
+let value = null;
+console.log(typeof value);
+
+console.log(
+  // value 변수의 타입이 'object'이면서(and) 값이 존재하지 않은 경우 'null'을 출력
+  // , 그것이 아니라면 'not null'을 출력
+  (typeof value === 'object' && !value) ? 'null' : 'not null'
+);
+
+// 4-2 undefined : 정의되지 않았다.
+// >> 변수가 선언되었으나, 아직 어떠한 값도 할당되지 않았을 때의 기본값
+
+let hello; // 변수 선언
+
+// clg : console.log()의 코드 스니펫(축약 단축키)
+console.log(hello);
+
+//? === 자료형 변환 ===
+// 문자열로 변환, 숫자형으로 변환, 논리형으로 변환
+
+// 1. String() 함수
+let num1 = 123;
+let num2 = 456;
+console.log(num1 + num2); // 579
+
+// 123456
+let StringNum1 = String(num1);
+let StringNum2 = String(num2);
+console.log(StringNum1 + StringNum2); // 123456
+
+// 2. Number() 함수
+
+let notANumber = '안녕하세요';  // 문자열
+console.log(Number(notANumber));  // NaN: Not A Number - 숫자로 변환할 수 없는 값을 변환 시도할 경우 
+
+console.log(Number(StringNum1));
+console.log(typeof(Number(StringNum1)));
+
+// 3. Boolean() 함수
+
+// false: false, 0, '', null, undefined, NaN
+// true: false값 제외한 모든 데이터를 true로 간주
+
+console.log(Boolean('hello'));  // true
+console.log(Boolean('')); // false
+console.log(Boolean(123));  // true
+console.log(Boolean(0));  // false
