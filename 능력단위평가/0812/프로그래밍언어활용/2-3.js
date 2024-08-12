b790bd3c2449f4cd69da3498bffa773f7f5fe31d
shifttag <ehdgus0837@naver.com>
2024-08-12 14:57:17 +0900

    20240812

--- 능력단위평가/0812/프로그래밍언어활용/2-3.js
+++ 능력단위평가/0812/프로그래밍언어활용/2-3.js
@@ -1,37 +1,54 @@
-// 도서 관리
-
-// 도서 추가
-// 도서 목록 출력
-// 도서 대여
-// 도서 반납
-let book = {
-  books: []
-};
-class Library{
-  constructor(id, title, author){
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.isAvailable = true;
-  }
-    add() {
-      const newBook = new book(this.books.length + 1, title, author);
-      this.books.push(newBook);
-      console.log(`${this.title}책이 추가되었습니다 (저자: ${this.author})`);
-    }
-
-    display(){
-      this.books.forEach(book => {
-        console.log(`${this.book.id}: ${this.book.title} 저자:${this.author} - ${this.book.isAvailable ? '대여 가능': '대여 불가'}`);
-      });
-    }
-
-    rent(id){
-      
-    }
-    
+// 
+const cars = [];
+class Car{
+  constructor(num, start, end){
+    this.start = start;
+    this.end = end;
+    this.num = num;
   }
 
-let busan = new Library(1, '12312', '12');
-busan.add();
-busan.display();
+  in(){
+    this.start = Date.now();
+    cars.push(this.num,this.start, this.end ='');
+  }
+  out(){
+    this.end = Date.now();
+    cars.push(this.num ,this.start, this.end);
+    const num1 = this.cars.find(car => car.num === num);
+    cars.map(car => {
+      if(num1 === this.num){
+        fee();
+      }
+      console.log(num1);
+  })
+  console.log(this.num);
+  console.log('등록된 차량이 없습니다');
+
+}
+
+  fee(){
+    timeout = (this.end - this.start) / 60;
+    if(timeout < 10){
+      console.log("공짜입니다.");
+    } else {
+      let fee1 = timeout * 1000;
+      console.log(`주차 요금은 : ${fee1}원 입니다.`);
+    }
+  }
+
+  
+}
+
+
+const car1 = new Car('7021');
+const car2 = new Car('1032');
+
+car1.in();
+car2.in();
+
+
+console.log(car1);
+console.log(car2);
+
+car1.out('7021');
+console.log(car1);
