6fe7131f6bf64dbc89c5c1295ab6e2a1ebc0553a
shifttag <ehdgus0837@naver.com>
2024-08-29 10:35:18 +0900

    20240829

---
+++ e_react/src/pages/b_hooks/a_UseState/ChildComponent.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type ChildProps = {
+  userData: UserType | undefined;
+};
+type UserType = {
+  username: string;
+  height: number;
+};
+export default function ChildComponent({ userData }: ChildProps) {
+  return (
+  <div>
+    <p>자식 컴포넌트 (부모로 부터 데이터를 전달받음)</p>
+    {userData &&(
+      // JSX에서 소괄호 내의 UI 구현부가 비워질 경우 오류
+      <>
+        <p>사용자 이름: {userData.username}</p>
+        <p>사용자 키: {userData.height}</p>
+      </>
+    )}
+  </div>
+  )
+}
--- e_react/src/pages/b_hooks/index.tsx
+++ e_react/src/pages/b_hooks/index.tsx
@@ -4,6 +4,12 @@ import UseState02 from "./a_UseState/UseState02";
 import UseState03 from "./a_UseState/UseState03";
 import UseState04 from "./a_UseState/UseState04";
 import UseState05 from "./a_UseState/UseState05";
+import ChildComponent from "./a_UseState/ChildComponent";
+import UseState06 from "./a_UseState/UseState06";
+import UseRef01 from "./b_UseRef/UseRef01";
+import UseRef02 from "./b_UseRef/UseRef02";
+import Practice01 from "./a_UseState/Practice01";
+import UseRef03 from "./b_UseRef/UseRef03";
 
 // 전체 파일 정렬
 // ctrl + a : 전체 선택
@@ -20,6 +26,14 @@ function Index() {
       <UseState03 />
       <UseState04 />
       <UseState05 />
+      <UseState06 />
+      <hr />
+      <Practice01 />
+      <hr />
+      <UseRef01 />
+      <UseRef02 />
+      <UseRef03 />
+
     </div>
   );
 }
